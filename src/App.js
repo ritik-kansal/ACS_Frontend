7fc58279c7801e8ac06751d8b758e64c47706c41
prabhdeep.singh <prabhdeep.singh@mastersunion.org>
2023-02-16 00:41:12 +0530

    addons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './Layouts/Header/Header';
 import Home from './Pages/Home/Home';
 
 function App() {
   return (
     <div className="appContainer">
+        <Header />
         <Routes>
           <Route exact path='/' element={<Home />}/>
         </Routes>
--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
+import './HeaderStyle.css';
 
 const Header = () => {
   return (
     <header>
         <div className="container">
-            
+            <nav className="navigationWrap">
+                <a href="/"><img src="/assets/svg/Logo.svg" alt="" /></a>
+                <ul className="navigation">
+                    <li className="navItem">
+                        <a href="/" className="navLink">Home</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">Services</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">Government</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">Oppertunities</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">About Us</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">Insight</a>
+                    </li>
+                    <li className="navItem">
+                        <a href="/" className="navLink">Contact Us</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </header>
   )
